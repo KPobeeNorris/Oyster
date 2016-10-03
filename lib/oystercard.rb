2bdb32f5610f11ba7c7f6aef60c257723c9abe2e
Rory Aitchison <rory.aitchison@gmail.com>
2016-10-03 17:04:40 +0100

    add balance function and associated tests

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -6,4 +6,8 @@ def initialize
   @balance = 0
 end
 
+def top_up(money)
+  @balance += money
+end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,16 @@ describe Oystercard do
   it 'have balance' do
     expect(subject).to respond_to :balance
   end
+
+  describe '#top up' do
+
+    describe 'tops up method exists'
+    it {is_expected.to respond_to :top_up}
+
+    describe 'top_up adds money'
+    oystercard = Oystercard.new
+    oystercard.top_up(10)
+    it {expect(oystercard.balance).to eq 10}
+  end
+
 end
