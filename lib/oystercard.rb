67fa7e19a3fd5ec5aa0654a234d534e8a81b461a
Kyaw Tun Sein <kyawtunsein.gla@gmail.com>
2016-10-03 17:40:18 +0100

    Add top_up method and implement limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,11 +2,15 @@ class Oystercard
 
 attr_reader :balance
 
+MONEY_LIMIT = 90
+
+
 def initialize
   @balance = 0
 end
 
 def top_up(money)
+  fail "Card limit is #{Oystercard::MONEY_LIMIT}." if money + @balance > Oystercard::MONEY_LIMIT
   @balance += money
 end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,10 +10,16 @@ describe Oystercard do
     describe 'tops up method exists'
     it {is_expected.to respond_to :top_up}
 
-    describe 'top_up adds money'
+    it 'top_up adds money' do
     oystercard = Oystercard.new
     oystercard.top_up(10)
-    it {expect(oystercard.balance).to eq 10}
+    expect(oystercard.balance).to eq 10
+    end
+
+    it 'limits top up value to £90' do
+      oystercard = Oystercard.new
+      expect{oystercard.top_up(91)}.to raise_error "Card limit is #{Oystercard::MONEY_LIMIT}."
+    end
   end
 
 end
