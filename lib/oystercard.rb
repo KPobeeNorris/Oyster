606168efb0e9e8dded867ec064acb72d8806e158
Kyaw Tun Sein <kyawtunsein.gla@gmail.com>
2016-10-03 16:41:55 +0100

    Add balance instance variable to Oystercard

---
+++ lib/oystercard.rb
@@ -0,0 +1,9 @@
+class Oystercard
+
+attr_reader :balance
+
+def initialize
+  @balance = 0
+end
+
+end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,7 @@
 require 'oystercard'
 
 describe Oystercard do
-
+  it 'have balance' do
+    expect(subject).to respond_to :balance
+  end
 end
