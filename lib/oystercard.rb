41d88d6966772ee4245d05dda6994973cd35eded
Rory Aitchison <rory.aitchison@gmail.com>
2016-10-04 18:08:35 +0100

    created working but ugly system of creating a journey history that stores each journey in an array as a pair of hash values. This journey is then stored in an array of similar entry-exit pairs. System accounts for incomplete journies in the history. Penalty fare system not yet created. No rspec tests for listed features; this is a spike

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,7 +2,7 @@ require_relative 'station'
 
 class Oystercard
 
-  attr_reader :balance, :entry_station
+  attr_reader :balance, :entry_station, :history
 
   MONEY_LIMIT = 90
   MINIMUM_BALANCE = 1
@@ -11,6 +11,8 @@ class Oystercard
 
   def initialize
     @balance = 0
+    @history = []
+    @current_journey = []
   end
 
   def top_up(money)
@@ -18,15 +20,30 @@ class Oystercard
     @balance += money
   end
 
-
-
   def touch_in(station)
     fail "Insufficient funds for journey" if @balance < MINIMUM_BALANCE
     @entry_station = station
+    if !@current_journey.empty? == 1
+      @current_journey << {exit_station: "INCOMPLETE"}
+      @history << @current_journey
+      @current_journey << {entry_station: station}
+    else
+      @current_journey << {entry_station: station}
+    end
   end
 
-  def touch_out
+  def touch_out(station)
     deduct(FARE)
+    if @current_journey.empty?
+      @current_journey << {entry_station: "INCOMPLETE"}
+      @current_journey << {exit_station: station.to_s}
+      @history << @current_journey
+      @current_journey = []
+    else
+      @current_journey << {exit_station: station.to_s}
+      @history << @current_journey
+      @current_journey = []
+    end
     @entry_station = nil
   end
 
