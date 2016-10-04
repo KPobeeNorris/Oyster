b5ef17a7caee0b5e74a1b2484a0e69d73fcb738d
Rory Aitchison <rory.aitchison@gmail.com>
2016-10-04 11:44:15 +0100

    completed challenge 8- added touch in and out and in_journey?

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,13 @@
 class Oystercard
 
-  attr_accessor :balance
+  attr_reader :balance
 
   MONEY_LIMIT = 90
 
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(money)
@@ -16,6 +17,18 @@ class Oystercard
 
   def deduct(fare)
     @balance-= fare
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+  def in_journey?
+    @in_journey
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,41 +1,65 @@
 require 'oystercard'
 
 describe Oystercard do
-  it 'have balance' do
-    expect(subject).to respond_to :balance
+  subject(:card) {described_class.new}
+
+  context 'balance' do
+    it 'have balance' do
+      expect(card).to respond_to :balance
+    end
   end
 
-  describe '#top up' do
+  context '#top up' do
 
     describe 'tops up method exists'
     it {is_expected.to respond_to :top_up}
 
     it 'top_up adds money' do
-    oystercard = Oystercard.new
-    oystercard.top_up(10)
-    expect(oystercard.balance).to eq 10
+    card.top_up(10)
+    expect(card.balance).to eq 10
     end
 
-    it 'limits top up value to £90' do
-      oystercard = Oystercard.new
-      expect{oystercard.top_up(91)}.to raise_error "Card limit is #{Oystercard::MONEY_LIMIT}."
+    it 'limits top up value MONEY_LIMIT' do
+      card.top_up(Oystercard::MONEY_LIMIT)
+      expect{card.top_up(1)}.to raise_error "Card limit is #{Oystercard::MONEY_LIMIT}."
     end
+
   end
 
-  describe '#money coming off card' do
+  context '#money coming off card' do
 
     it 'deducts money' do
-      oystercard = Oystercard.new
-      expect(oystercard).to respond_to :deduct
+      expect(card).to respond_to :deduct
     end
 
-    it 'deducts £2 per journey' do
-      oystercard = Oystercard.new
-      oystercard.top_up(10)
-      oystercard.deduct(2)
-      expect(oystercard.balance).to eq 8
+    it 'deducts (fare(£2)) per journey' do
+      card.top_up(10)
+      card.deduct(2)
+      expect(card.balance).to eq 8
     end
 
   end
+
+  context 'touching in and out' do
+
+    it 'responds to in_journey' do
+      expect(card.in_journey?).to eq(true).or eq(false)
+    end
+
+    it 'touching in registers that the card is in journey' do
+      card.touch_in
+      expect(card.in_journey?).to eq true
+    end
+
+    it 'touching out registers the card as no longer being in journey' do
+      card.touch_in
+      card.touch_out
+      expect(card.in_journey?).to eq false
+    end
+
+  end
+
+
+
 
 end
